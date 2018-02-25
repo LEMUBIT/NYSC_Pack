9f64733ebdf21e0cec3a66aa53cce0f4069f03e6
lemuelc7 <lemuelcco@gmail.com>
2018-02-25 20:14:25 +0100

    Work on Util class

--- app/src/main/java/com/lemuel/lemubit/nyscpack/Login.java
+++ app/src/main/java/com/lemuel/lemubit/nyscpack/Login.java
@@ -1,7 +1,23 @@
+/*
+Copyright 2018 OGBUNUDE CHIBUEZE LEMUEL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
 package com.lemuel.lemubit.nyscpack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +25,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
--- app/src/main/java/com/lemuel/lemubit/nyscpack/Util/Open.java
+++ app/src/main/java/com/lemuel/lemubit/nyscpack/Util/Open.java
@@ -1,17 +1,32 @@
+/*
+Copyright 2018 OGBUNUDE CHIBUEZE LEMUEL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
 package com.lemuel.lemubit.nyscpack.Util;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-/**
- * Created by charl on 25/02/2018.
- */
-
+//Class with methon 'OpenActivity which is to help reduce boilerplate code when opening new activity
 public class Open {
-    public static void OpenActivity(Context context, Class<?> cls, Bundle bundle)
-    {
-        Intent intent=new Intent(context,cls);
+    public static void OpenActivity(Context context, Class<?> cls, Bundle bundle) {
+
+        Intent intent = new Intent(context, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
         context.startActivity(intent);
     }
 }
