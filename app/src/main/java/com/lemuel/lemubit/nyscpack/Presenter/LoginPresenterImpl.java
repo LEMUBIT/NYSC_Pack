14a6f7632d860377f28058ee0a927fd647eb36af
lemuelc7 <lemuelcco@gmail.com>
2018-02-26 11:34:45 +0100

    Work on MVP design

--- app/src/main/java/com/lemuel/lemubit/nyscpack/Presenter/LoginPresenterImpl.java
+++ app/src/main/java/com/lemuel/lemubit/nyscpack/Presenter/LoginPresenterImpl.java
@@ -20,18 +20,20 @@ import android.widget.ArrayAdapter;
 
 import com.lemuel.lemubit.nyscpack.View.LoginView;
 import com.lemuel.lemubit.nyscpack.Realm.RealmModel;
-
 import io.realm.Realm;
-import io.realm.RealmResults;
+
 
 
 public class LoginPresenterImpl implements LoginPresenter {
     LoginView loginView;
     Realm realm;
+    RealmModel realmModel = new RealmModel();
+    Context context;
 
     public LoginPresenterImpl(Context context, LoginView loginView) {
         // Initialize Realm
         Realm.init(context);
+        this.context = context;
         this.loginView = loginView;
         realm = Realm.getDefaultInstance();
     }
@@ -39,25 +41,15 @@ public class LoginPresenterImpl implements LoginPresenter {
     @Override
     public void saveEmail(String email) {
         //Save email
-        realm.beginTransaction();
-        RealmModel user = realm.createObject(RealmModel.class);
-        user.setEmail(email);
-        realm.commitTransaction();
+        realmModel.saveNewEmail(email, context);
     }
 
     @Override
     public void getSavedEmails(Context context) {
-        RealmResults<RealmModel> results = realm.where(RealmModel.class).distinctValues("email").findAll();
-        String[] resultArray = new String[results.size()];
-        int increment = 0;
-        for (RealmModel s : results) {
-            resultArray[increment] = s.getEmail();
-            increment++;
-        }
+
         //Get Emails from Realm into auto-correct text
         ArrayAdapter<String> adapter = new ArrayAdapter(context,
-                android.R.layout.simple_dropdown_item_1line, resultArray);
-
+                android.R.layout.simple_dropdown_item_1line, realmModel.getEmails(context));
         loginView.onSetAutoCompleterText(adapter);
     }
 }
--- app/src/main/java/com/lemuel/lemubit/nyscpack/Realm/RealmModel.java
+++ app/src/main/java/com/lemuel/lemubit/nyscpack/Realm/RealmModel.java
@@ -15,10 +15,14 @@ limitations under the License.*/
 
 package com.lemuel.lemubit.nyscpack.Realm;
 
+import android.content.Context;
+
+import io.realm.Realm;
 import io.realm.RealmObject;
+import io.realm.RealmResults;
 
 
-public class RealmModel extends RealmObject{
+public class RealmModel extends RealmObject {
     private String email;
 
     public String getEmail() {
@@ -27,5 +31,27 @@ public class RealmModel extends RealmObject{
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void saveNewEmail(String email, Context context) {
+        Realm.init(context);
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        RealmModel user = realm.createObject(RealmModel.class);
+        user.setEmail(email);
+        realm.commitTransaction();
+    }
+
+    public String[] getEmails(Context context) {
+        Realm.init(context);
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<RealmModel> results = realm.where(RealmModel.class).distinctValues("email").findAll();
+        String[] resultArray = new String[results.size()];
+        int increment = 0;
+        for (RealmModel s : results) {
+            resultArray[increment] = s.getEmail();
+            increment++;
+        }
+        return resultArray;
     }
 }
